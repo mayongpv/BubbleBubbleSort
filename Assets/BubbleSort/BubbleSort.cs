395955dc78c1b8e2b94a5070a4bd306657c3496a
mayongpv <82883541+mayongpv@users.noreply.github.com>
2021-05-22 23:57:20 +0900

    1번 로직 정리해보기

---
+++ Assets/BubbleSort/BubbleSort.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    /*수를 나열한다.
+     왼쪽부터 수를 두개씩 비교한다.
+     큰 수를 오른쪽으로 보낸다. -> 왼쪽 수가 오른쪽 보다 크면 위치를 바꾼다.
+     그럼 가장 큰 수가 맨 오른쪽으로 간다.
+     한바퀴가 끝나면 가장 오른쪽에 있는 수를 고정한다.
+     나머지 숫자들도 같은 방법으로 반복한다.
+     기존 숫자가 3, 2, 4 라면 실행 횟수 : 1, 2 번이니까 실행 횟수 는 (수의 개수 -1) 이다.
+     수의 개수 -1 만큼 반복한다.
+     완성!
+     */
+}
