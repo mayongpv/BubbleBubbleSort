462c1a3c6dd5d7b9765034ebd82e97270392cdcd
mayongpv <82883541+mayongpv@users.noreply.github.com>
2021-05-23 01:10:17 +0900

    혼자 생각해보기&Console

--- Assets/BubbleSort/BubbleSort.cs
+++ Assets/BubbleSort/BubbleSort.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class BubbleSort : MonoBehaviour
 {
+    //할일 적기
     /*수를 나열한다.
+     * -> 리스트 사용
      왼쪽부터 수를 두개씩 비교한다.
      큰 수를 오른쪽으로 보낸다. -> 왼쪽 수가 오른쪽 보다 크면 위치를 바꾼다.
      그럼 가장 큰 수가 맨 오른쪽으로 간다.
@@ -14,4 +16,30 @@ public class NewBehaviourScript : MonoBehaviour
      수의 개수 -1 만큼 반복한다.
      완성!
      */
+    public void Start()
+    {
+        //1. 수를 나열한다. -> 리스트 이용
+
+        List<int> bubblesort = new List<int>()
+        { 3, 5, 10, -2, 24};
+        
+        //2. 왼쪽부터 수를 두개씩 비교한다.
+        //큰 수를 오른쪽으로 보낸다. -> 왼쪽 수가 오른쪽 보다 크면 위치를 바꾼다.
+        if (bubblesort[0] > bubblesort[1])
+        {
+            //바꾼다? 즉, 오른쪽에 있는 수를 왼쪽에 넣고 왼쪽에 있는 수를 오른쪽으로 넣는다?
+            System.Console.WriteLine(bubblesort[0] = bubblesort[1]);
+            System.Console.WriteLine(bubblesort[1] = bubblesort[0]);
+            Debug.Log(bubblesort[0] = bubblesort[1]);
+            Debug.Log(bubblesort[1] = bubblesort[0]);
+
+        }
+    }
+
+
+
 }
+
+
+
+
---
+++ 콘솔작업/Before.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Before
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Hello World!");
+
+            List<int> bubblesort = new List<int>()
+        { 3, 5, 10, -2, 24};
+            Console.WriteLine(bubblesort[3]);
+
+            //2. 왼쪽부터 수를 두개씩 비교한다.
+            //큰 수를 오른쪽으로 보낸다. -> 왼쪽 수가 오른쪽 보다 크면 위치를 바꾼다.
+            if (bubblesort[0] > bubblesort[1])
+            {
+
+                //바꾼다? 즉, 오른쪽에 있는 수를 왼쪽에 넣고 왼쪽에 있는 수를 오른쪽으로 넣는다?
+                Console.WriteLine(bubblesort[0] = bubblesort[1]);
+                Console.WriteLine(bubblesort[1] = bubblesort[0]);
+            }
+            else
+            {
+                Console.WriteLine(bubblesort[0]);
+                Console.WriteLine(bubblesort[1]);
+            }
+
+        }
+    }
+}
+
